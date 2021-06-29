0a6aa8d6c55817cde786a7f0467cbe792b704700
watanabe@tiffa <watanabe.everystudio@gmail.com>
2021-06-29 21:41:13 +0900

    kvsを標準搭載します

---
+++ Scripts/Csvutility/KVS.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace anogamelib {
+
+    public class KVSParam : CsvDataParam
+    {
+        public string key;
+        public string value;
+    }
+
+    public class KVS : CsvData<KVSParam>
+    {
+    }
+}
