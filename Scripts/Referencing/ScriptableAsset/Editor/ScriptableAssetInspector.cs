66f1fc4f0856e30fbab799b92188560b97d2b495
watanabe@tiffa <watanabe.everystudio@gmail.com>
2021-12-15 22:44:57 +0900

    アセットリファレンス処理を追加

---
+++ Scripts/Referencing/ScriptableAsset/Editor/ScriptableAssetInspector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace anogamelib
+{
+    [CustomEditor(typeof(ScriptableAsset), true)]
+    public class ScriptableAssetInspector : Editor
+    {
+        private string currentGUID;
+
+        public override void OnInspectorGUI()
+        {
+            if (string.IsNullOrEmpty(currentGUID))
+            {
+                currentGUID = $"GUID: {((IReferenceableAsset)target).GetGuid()}";
+            }
+            EditorGUILayout.TextArea(currentGUID, EditorStyles.centeredGreyMiniLabel);
+
+            base.DrawDefaultInspector();
+        }
+    }
+}
