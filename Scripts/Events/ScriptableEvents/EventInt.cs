84d2fd7f5c2a7a9cd8c8116fac54eab67ea910f2
watanabe@tiffa <watanabe.everystudio@gmail.com>
2022-01-09 16:33:06 +0900

    GameObjectのイベントも追加

--- Scripts/Events/ScriptableEvents/EventInt.cs
+++ Scripts/Events/ScriptableEvents/EventInt.cs
@@ -6,6 +6,9 @@ namespace anogamelib
 	[CreateAssetMenu(menuName = "Events/Int Event")]
 	public class EventInt : ScriptableEvent<int>
 	{
-
+	}
+	[CreateAssetMenu(menuName = "Events/GameObject Event")]
+	public class EventGameObject : ScriptableEvent<GameObject>
+	{
 	}
 }
